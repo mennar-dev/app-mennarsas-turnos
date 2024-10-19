fbbb843644a8677a2974464809ae86e82f8b10f4
Edwin Burbano Luna <auxiliar.sistemas@mennarsas.com.co>
2024-10-19 10:54:01 -0500

    Commit inicial del proyecto

---
+++ app/src/main/java/com/mennarsas/app_mennar_turnos/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mennarsas.app_mennar_turnos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mennarsas.app_mennar_turnos.ui.theme.AppmennarturnosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppmennarturnosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "App Mennar turnos",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppmennarturnosTheme {
+        Greeting("MENNAR SAS")
+    }
+}
