c2b06b3faa67b6ac69e0d2ded73165aeba916deb
Edwin Burbano Luna <auxiliar.sistemas@mennarsas.com.co>
2024-10-19 10:59:50 -0500

    Probando la sincronización de la rama development

--- app/src/main/java/com/mennarsas/app_mennar_turnos/MainActivity.kt
+++ app/src/main/java/com/mennarsas/app_mennar_turnos/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppmennarturnosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "App Mennar turnos",
+                        name = "Aplicación móvil para gestionar turnos",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
